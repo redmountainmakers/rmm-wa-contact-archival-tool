e81e01306baec72e9169fc00c3465a04576b9b08
Creidhne86 <mcbehling86@gmail.com>
2025-01-03 12:27:23 -0600

    Update archival_script.py

--- archival_script.py
+++ archival_script.py
@@ -131,7 +131,7 @@ def num_members(access_token):
     if not account_id:
         return None
 
-    filter_query = f"$filter='Membership Enabled' eq 'yes'"
+    filter_query = f"$filter='Member' eq 'True'"
     contacts_url = f"{api_base_url}/accounts/{account_id}/contacts?$async=false&{filter_query}"
     
     contacts_response = requests.get(contacts_url, headers=headers)
@@ -221,7 +221,7 @@ def return_archival_candidates(access_token):
     if not account_id:
         return None
 
-    filter_query = f"$filter='Member' eq 'False' and 'IsArchived' eq 'False' and 'Balance' eq '0.0'"
+    filter_query = f"$filter='Member' ne 'True' and 'IsArchived' eq 'False' and 'Balance' eq '0.0'"
     contacts_url = f"{api_base_url}/accounts/{account_id}/contacts?$async=false&{filter_query}"
     
     contacts_response = requests.get(contacts_url, headers=headers)
